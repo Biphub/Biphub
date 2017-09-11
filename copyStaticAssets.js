f1a2bbf521c6dfd51e648feae23e924baafda745
Jason Shin <visualbbasic@gmail.com>
2017-09-11 21:55:55 +1000

    working on schemas

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -1,5 +1,5 @@
 var shell = require('shelljs');
 
-shell.cp('-R', 'src/public/js/lib', 'dist/public/js/');
-shell.cp('-R', 'src/public/fonts', 'dist/public/');
-shell.cp('-R', 'src/public/images', 'dist/public/');
+shell.cp('-R', 'core/public/js/lib', 'dist/public/js/');
+shell.cp('-R', 'core/public/fonts', 'dist/public/');
+shell.cp('-R', 'core/public/images', 'dist/public/');
