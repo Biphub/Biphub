121811ac5dcbaf948f7a3bbc99c352598d57d3d8
Jason Shin <visualbbasic@gmail.com>
2017-12-05 22:18:24 +1100

    nginx fixed

--- client/src/index.js
+++ client/src/index.js
@@ -30,7 +30,7 @@ const client = new ApolloClient({
   //  `/graphql` endpoint on the same host
   // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
   // to a different host
-  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
+  link: new HttpLink({ uri: process.env.GRAPHQL_URI || '/graphql' }),
   cache: new InMemoryCache()
 })
 
